8748418cbbda8828bea25378c2943ddd6aa18466
lian saed <layan4saed@gmail.com>
2024-01-16 01:08:20 +0200

    implementing New Arrivals at home page

--- src/components/home/NewArraival.js
+++ src/components/home/NewArraival.js
@@ -1,5 +1,100 @@
-function NewArrivals() {
-    return (  <></>);
+import styled from 'styled-components';
+import ProductCard from '../shared/ProductCard';
+
+const SharedTextStyles = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap');
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+`;
+
+const ArrivalLayout = styled.div`
+  width: 78.5rem;
+  height: 29.6875rem;
+  flex-shrink: 0;
+`;
+
+const TopStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const CardsLayout = styled.div`
+  display: inline-flex;
+  height: 25.48175rem;
+  align-items: flex-start;
+  gap: 2.5rem;
+  flex-shrink: 0;
+`;
+
+const TitleStyle = styled.div`
+  display: flex;
+  width: 13.0625rem;
+  height: 2.72138rem;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+`;
+
+const TitleTextStyle = styled(SharedTextStyles)`
+  color: var(--Dark, #13101E);
+  font-size: 2.125rem;
+  line-height: 2.75rem;
+`;
+
+const ViewAllLayout = styled.div`
+  width: 6.4375rem;
+  height: 2.75rem;
+  flex-shrink: 0;
+`;
+
+const ViewAllButton = styled.div`
+  display: inline-flex;
+  padding: 0.625rem 0.5rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const ButtonText = styled(SharedTextStyles)`
+  color: var(--Primary, #1B4B66);
+  text-align: center;
+  font-size: 0.875rem;
+  line-height: 1.5rem; 
+`;
+
+function NewArrivals({ cards }) {
+  return (
+    <ArrivalLayout>
+      <TopStyle>
+        <TitleStyle><TitleTextStyle>New Arrivals</TitleTextStyle></TitleStyle>
+        <ViewAllLayout>
+          <ViewAllButton>
+            <ButtonText>View All</ButtonText>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+              <path d="M9 4.5L16.5 12L9 19.5" stroke="#13101E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </ViewAllButton>
+        </ViewAllLayout>
+      </TopStyle>
+      <CardsLayout>
+        {cards.map((item) => (
+          <ProductCard
+            key={item.productId}
+            image={item.image}
+            productName={item.productName}
+            productDescreption={item.productDescreption}
+            discount={item.discount}
+            showRating={item.showRating}
+            price={item.price}
+            showOldPrice={item.showOldPrice}
+            ratersNumber={item.ratersNumber}
+          />
+        ))}
+      </CardsLayout>
+    </ArrivalLayout>
+  );
 }
 
 export default NewArrivals;
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Container } from "./Container";
+import NewArrivals from "../components/home/NewArraival";
 /**
  * import ProductCard from "../components/shared/product-card/ProductCard";
  * import pinkBagImage from '../assets/newArrivals/pink-bag-small.png';
@@ -10,8 +11,32 @@ import { Container } from "./Container";
  */
 
 const Layout = () => {
+  let tempObj = [
+    {
+      image: "../assets/newArrivals/pink-bag-small.png",
+      productName: "Grande",
+      productDescreption: "Blossom Pouch",
+      discount: 0.5,
+      showRating: false,
+      price: 39.49,
+      showOldPrice: false,
+      ratersNumber: 43
+    },
+    {
+      image: "../assets/newArrivals/pink-bag-small.png",
+      productName: "Grande",
+      productDescreption: "Blossom Pouch",
+      discount: 0.5,
+      showRating: false,
+      price: 39.49,
+      showOldPrice: false,
+      ratersNumber: 43
+    }
+  ]
   return (
     <Container>
+      <NewArrivals cards={tempObj} />
+      <br />
       <Header />
       <main>
         <Outlet />
